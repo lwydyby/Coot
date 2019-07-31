823b907d787b1a641c27d14d66755d301d01cc82
lauix <lauixData@gmail.com>
2019-07-31 10:31:54 +0800

    更新Coot

--- core/queue/defs.go
+++
@@ -1,3 +0,0 @@
-package queue
-
-type typeStrChan chan string
--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 )
 
 func main() {
-	setting.RunWeb("localhost:9000")
 	args := os.Args
 	if args == nil || len(args) < 2 {
 		setting.Help()
